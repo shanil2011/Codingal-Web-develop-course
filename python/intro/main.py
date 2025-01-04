c3d2343ff90adf22605d088a199865d9397b507f
shanil2011 <shoriatpbl17@gmail.com>
2025-01-04 17:22:10 +0600

    intro

--- python/intro/main.py
+++ python/intro/main.py
@@ -8,3 +8,17 @@ print("I am a Python programmer.")
 
 #Performing and printing a simple calculation
 print("The result of 3+5 is:", 3 + 5)
+
+#Print a sentence
+print("Welcome to the world of python programming!")
+
+#Print a Number
+print(4)
+
+#Next line after print
+print("Hello , world! \n")
+
+#Print all the keywords present in python
+print("Keywords in Python are:....")
+import keyword
+print(keyword.kwlist)
