5de08f90d2afd078e74341a8b0d129e6c48f969f
shanil2011 <shoriatpbl17@gmail.com>
2025-01-04 17:13:37 +0600

    intro

---
+++ python/intro/main.py
@@ -0,0 +1,10 @@
+print("My name is shanil.")
+
+#Printing a greeting message
+print("Hello, World!")
+
+#Printing a fun fact about myself
+print("I am a Python programmer.")
+
+#Performing and printing a simple calculation
+print("The result of 3+5 is:", 3 + 5)
