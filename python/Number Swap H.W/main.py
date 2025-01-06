ff8abd01170b4623e2a04935c860c0b3932c235d
shanil2011 <shoriatpbl17@gmail.com>
2025-01-06 16:49:58 +0600

    numberchangehw

---
+++ python/Number Swap H.W/main.py
@@ -0,0 +1,13 @@
+x = input("Enter the value of x: ")
+y = input("Enter the value of y: ")
+z = input("Enter the value of z: ")
+
+
+temp = x
+x = y
+y = z
+y = temp
+
+print("The value of x after swapping:", x)
+print("The value of y after swapping:", y) 
+print("The value of z after swapping:", z) 
