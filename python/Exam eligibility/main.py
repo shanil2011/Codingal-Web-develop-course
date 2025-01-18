67e548d8ed2187c754c7be05fdb8bfd51917bc61
shanil2011 <shoriatpbl17@gmail.com>
2025-01-18 15:34:34 +0600

    eligibility

---
+++ python/Exam eligibility/main.py
@@ -0,0 +1,10 @@
+
+print("There are 217 working days in a year.") # This is a comment
+num = int(input("Enter the number of those days when you are present in class: "))
+
+if num>= 120:
+    print("You can join in the next semester.")
+else:
+    print("You Can not join in the next semester.")
+
+
