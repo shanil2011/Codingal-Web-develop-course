28ff0b3ccd0414e7de3fdf664a9da24bc78ff056
shanil2011 <shoriatpbl17@gmail.com>
2025-01-20 16:13:15 +0600

    Star

---
+++ python/Star/main.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.Screen().bgcolor("blue")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+turtle.done()
