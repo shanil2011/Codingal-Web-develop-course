40eb4bfd064682909498f028959be41a825debdc
shanil2011 <shoriatpbl17@gmail.com>
2025-01-20 16:19:53 +0600

    star2

--- python/Star/main.py
+++ python/Star/main.py
@@ -15,4 +15,14 @@ board.penup()
 board.right(150)
 board.forward(50)
 
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
 turtle.done()
