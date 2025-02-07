4392314877f5a7159bd84c87e4c0acca9ffb8e0b
shanil2011 <shoriatpbl17@gmail.com>
2025-02-07 16:46:19 +0600

    studentdetails&

--- python/Student Details/main.py
+++ python/Student Details/main.py
@@ -1,16 +1,16 @@
 class Person:
-    def __init__(self, fname, lname):
-     self.firstname = fname
-     self.lastname  = lname 
-    
-    def printname(self):
-       print(self.firstname , self.lastname)
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
 
 class Student(Person):
-    def __init__(self, fname, lname, year):
-     super().__init__(fname, lname)
-     self.graduationyear = year
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
 
-x = Student("Joey", "king", 2021)
-x.printname
+x = Student("Joey", "King", 2021)
+x.printname()
 print(x.graduationyear)
