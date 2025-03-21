634623b4eedeb608c8bd8f5c7078254901d33b4e
shanil2011 <shoriatpbl17@gmail.com>
2025-03-21 11:43:06 +0600

    space invaders

---
+++ python/a/main.py
@@ -0,0 +1,7 @@
+import random
+
+options = ("Smart","Stupid","Good","Bad")
+print("Shanil is ",random.choice(options))
+print("Sawwaf is ",random.choice(options))
+print("Subita is ",random.choice(options))
+print("Samita is ",random.choice(options))
