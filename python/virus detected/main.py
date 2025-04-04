7cf91c2d0509c72775bf36157dd9333385f5bb64
shanil2011 <shoriatpbl17@gmail.com>
2025-04-04 11:49:08 +0600

    virus foundedeee

--- python/virus detected/main.py
+++ python/virus detected/main.py
@@ -7,7 +7,7 @@ root.geometry("200x200")
 def msg():
     messagebox.showinfo("Alert", "stop! Virus founded")
 
-button = Button(root, text="Click me", command=msg)
+button = Button(root, text="Scan for virus", command=msg)
 button.place(x=40, y=80)
 
 root.mainloop()
