9cbdd232b8865549975a1b82b43312d84f8ae48d
shanil2011 <shoriatpbl17@gmail.com>
2025-04-12 13:21:12 +0600

    mother father

--- python/password strength checker/main.py
+++ python/password strength checker/main.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 enter = input("Enter something: ")
 running = True
-print("Before we start this program , this might take longer than expected , please be patient.")
+print("Before we start this program , this might take longer than expected , please be patient.You have to click enter multiple time.")
 
 num1 = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 num2 = 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
