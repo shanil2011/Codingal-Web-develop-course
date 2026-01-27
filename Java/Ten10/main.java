9dd8061ff4c26ab8e35eb89094c57fd7f1e6f225
shanil2011 <shoriatpbl17@gmail.com>
2026-01-27 17:32:55 +0600

    ten10

---
+++ Java/Ten10/main.java
@@ -0,0 +1,14 @@
+class main{
+    public static void main(String[] args) {
+        for(int i=10; i>1; i--){
+            System.out.println("The value of i is: " + i);
+    }
+    System.out.println("This is the end of The I loop");
+    int j=0;
+    while(j<10){
+        System.out.println("The value of j is: " + j);
+        j++;
+    }
+    System.out.println("This is the start of The J loop");
+  }   
+}
