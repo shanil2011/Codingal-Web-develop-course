803e562e7d1a4983da85604027216b18b3aaa13c
shanil2011 <shoriatpbl17@gmail.com>
2026-01-27 17:45:25 +0600

    Hellouser

---
+++ Java/Hello User/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class main{
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+       System.out.println("Hello user please enter your name:");
+       String name=sc.nextLine();
+       System.out.println("Enter your lucky number :");
+       int lucky=sc.nextInt();
+       System.out.println("Hello "+name+" your lucky number is "+lucky+ " showing you are really hardworking, KEEP IT UP.");
+    }
+}
