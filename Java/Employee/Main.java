c82cb9a3abed4981ee8b8666cdfafecd7b28f531
shanil2011 <shoriatpbl17@gmail.com>
2026-02-02 18:47:42 +0600

    MyEmployeeIs'ntWorking

---
+++ Java/Employee/Main.java
@@ -0,0 +1,28 @@
+class Employee{
+    int empno;
+    String name;
+    float sal;
+
+    Employee(){
+        System.out.println("********");
+        empno = 101;
+        name = "Shanil";
+        sal = 90000f;
+    }
+
+    void displayDetails(){
+        System.out.println(empno + " | " + name + " | " + sal);
+    }
+}
+class Main {
+    public static void main(String[] args)
+        {
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee();
+            Employee emp3 = new Employee();
+
+            emp1.displayDetails();
+            emp2.displayDetails();
+            emp3.displayDetails();
+    }
+}
